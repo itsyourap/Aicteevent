fe7bec0a6a67df80a29a7b847a52e70d93acaba9
IndraniSom <indranisom39@gmail.com>
2024-11-28 10:56:46 +0530

    code refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./registration.html",
-    "about.html",
-    "contact.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "*"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
